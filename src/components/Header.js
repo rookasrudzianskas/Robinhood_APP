78a05c304cb1df273d87c43982fbeb79f5c01bcc
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 08:47:22 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
     return (
         <div className="header__wrapper">
             <div className="header__logo">
-                TEST
+                <img src="https://raw.githubusercontent.com/CleverProgrammers/robinhood-clone/69f8244e276928c04fffbd3324ee21c6c82d28e6/src/robinhood.svg" width={25} alt=""/>
+
+
             </div>
         </div>
     );
