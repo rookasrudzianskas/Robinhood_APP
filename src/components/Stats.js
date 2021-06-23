a932e044438ce448f10d5d87533069580eb6f669
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 11:23:42 +0300

    Rokas

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,7 +4,28 @@ import "./styles/Stats.css";
 const Stats = () => {
     return (
         <div className="stats">
-            
+            <div className="stats__container">
+                <div className="stats__header">
+                    <p>Stocks</p>
+                </div>
+
+                <div className="stats__content">
+                    <div className="stats__rows">
+
+                    </div>
+                </div>
+
+                <div className="stats__header">
+                    <p>Lists</p>
+                </div>
+
+                <div className="stats__content">
+                    <div className="stats__rows">
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
