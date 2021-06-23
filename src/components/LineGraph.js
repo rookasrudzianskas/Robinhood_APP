e0f7dafd60f5937a074d171814f37a637f2cc1b0
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 10:21:02 +0300

    Rokas

---
+++ src/components/LineGraph.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles/LineGraph.css";
+import {Line} from 'react-chartjs-js';
+
+const LineGraph = () => {
+    return (
+        <div className="lineGraph">
+            <Line
+
+            />
+        </div>
+    );
+};
+
+export default LineGraph;
--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./styles/NewsFeed.css";
+import LineGraph from "./LineGraph";
 
 const NewsFeed = () => {
     return (
@@ -9,6 +10,10 @@ const NewsFeed = () => {
                     <div className="newsFeed__portfolio">
                         <h1>$114,3432</h1>
                         <p>+$44.34 (+0.04%) Today</p>
+                    </div>
+
+                    <div className="newsFeed__chart">
+                        <LineGraph />
                     </div>
                 </div>
             </div>
