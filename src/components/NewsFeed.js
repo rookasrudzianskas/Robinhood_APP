6ad29e58bde7d8b036cf7599deeb13b6a8883d64
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 10:08:14 +0300

    Rokas

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -5,7 +5,12 @@ const NewsFeed = () => {
     return (
         <div className="newsFeed">
             <div className="newsFeed__container">
-
+                <div className="newsFeed__chartSection">
+                    <div className="newsFeed__portfolio">
+                        <h1>$114,3432</h1>
+                        <p>+$44.34 (+0.04%) Today</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
