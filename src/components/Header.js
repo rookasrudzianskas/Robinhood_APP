11d2922d0aedc8911fbb1c20195a525db8b3a8c7
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 09:04:13 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,12 @@ const Header = () => {
         <div className="header__wrapper">
             <div className="header__logo">
                 <img src="https://raw.githubusercontent.com/CleverProgrammers/robinhood-clone/69f8244e276928c04fffbd3324ee21c6c82d28e6/src/robinhood.svg" width={25} alt=""/>
+            </div>
 
-
+            <div className="header__search">
+                <div className="header__searchContainer">
+                    <input type="text" placeholder="Search"/>
+                </div>
             </div>
         </div>
     );
