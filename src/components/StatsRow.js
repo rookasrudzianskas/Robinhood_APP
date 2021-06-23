1e579f4e1891df9bf7f5235ff445275ab0c67435
Rokas <rookas.rudzenskas@gmail.com>
2021-06-23 11:54:20 +0300

    Rokas

--- src/components/StatsRow.js
+++ src/components/StatsRow.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const StatsRow = () => {
+const StatsRow = (props) => {
     return (
-        <div className="statsRow">
-            
+        <div className="row">
+            <div className="row__intro">
+                <h1>AAPL</h1>
+                <p>200 shares</p>
+            </div>
+            <div className="row__chart">
+                {/*<img src={StockChart} height={16}/>*/}
+            </div>
+            <div className="row__numbers">
+                <p className="row__price">$200</p>
+                <p className="row__percentage"> +200%</p>
+            </div>
         </div>
     );
 };
